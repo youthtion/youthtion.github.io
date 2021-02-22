a2a631f1dd8c215b3af7b5af576fe040ef121365
Uth <46614539+youthtion@users.noreply.github.com>
2021-02-23 04:22:18 +0900

    Add files via upload

--- nihongo/QAList.js
+++ nihongo/QAList.js
@@ -1,4 +1,5 @@
 var QAList = new Array(
+//-----------------------------------------------------V-----------------------------------------------------
 {que:"開く"				,ans:"あく"			,tip:"(something) open"						,group:1	,accr:0},
 {que:"閉まる"			,ans:"しまる"		,tip:"(something) close"					,group:1	,accr:0},
 {que:"開ける"			,ans:"あける"		,tip:"to open"								,group:2	,accr:0},
@@ -13,7 +14,7 @@ var QAList = new Array(
 {que:"寝る"				,ans:"ねる"			,tip:"go to bed"							,group:6	,accr:0},
 {que:"押す"				,ans:"おす"			,tip:"to push"								,group:7	,accr:0},
 {que:"引く"				,ans:"ひく"			,tip:"pull/draw"							,group:7	,accr:0},
-{que:"降りる"			,ans:"おりる"		,tip:"to get off;to get out <of>"			,group:8	,accr:0},	//上がる/上る/登る
+{que:"降りる"			,ans:"おりる"		,tip:"to get off/to get out (of)"			,group:8	,accr:0},	//上がる/上る/登る
 {que:"乗る"				,ans:"のる"			,tip:"take/ride on"							,group:8	,accr:0},
 {que:"終わる"			,ans:"おわる"		,tip:"(something) be finished"				,group:9	,accr:0},
 {que:"始まる"			,ans:"はじまる"		,tip:"(something) begin"					,group:9	,accr:0},
@@ -22,7 +23,7 @@ var QAList = new Array(
 {que:"貸す"				,ans:"かす"			,tip:"to lend"								,group:10	,accr:0},
 {que:"書く"				,ans:"かく"			,tip:"to write"								,group:11	,accr:0},
 {que:"読む"				,ans:"よむ"			,tip:"read"									,group:11	,accr:0},
-{que:"掛ける"			,ans:"かける"		,tip:"to wear(a glass)/to telephone"		,group:12	,accr:0},	//外す
+{que:"掛ける"			,ans:"かける"		,tip:"to wear(a glass)"						,group:12	,accr:0},	//外す
 {que:"切る"				,ans:"きる"			,tip:"cut"									,group:12	,accr:0},	//結ぶ/続ける/繋ぐ
 {que:"被る"				,ans:"かぶる"		,tip:"put on (hat)"							,group:13	,accr:0},
 {que:"着る"				,ans:"きる"			,tip:"to put (tops) on/to wear (tops)"		,group:13	,accr:0},
@@ -51,6 +52,7 @@ var QAList = new Array(
 {que:"行う"				,ans:"おこなう"		,tip:"to do/to operate"						,group:0	,accr:0},
 {que:"覚える"			,ans:"おぼえる"		,tip:"to memorize"							,group:0	,accr:0},	//忘れる
 {que:"泳ぐ"				,ans:"およぐ"		,tip:"to swim"								,group:0	,accr:0},
+{que:"掛ける"			,ans:"かける"		,tip:"to telephone"							,group:0	,accr:0},	//外す
 {que:"消える"			,ans:"きえる"		,tip:"be extinguished"						,group:0	,accr:0},	//現れる/点る/灯る/燃える/点く/熾る
 {que:"答える"			,ans:"こたえる"		,tip:"answer"								,group:0	,accr:0},	//問う/尋ねる
 {que:"困る"				,ans:"こまる"		,tip:"be in trouble"						,group:0	,accr:0},
@@ -85,18 +87,121 @@ var QAList = new Array(
 {que:"呼ぶ"				,ans:"よぶ"			,tip:"call"									,group:0	,accr:0},
 {que:"渡す"				,ans:"わたす"		,tip:"hand over"							,group:0	,accr:0},
 {que:"渡る"				,ans:"わたる"		,tip:"cross"								,group:0	,accr:0},
+//----------------------------------------------------ADJ----------------------------------------------------
+{que:"暖かい"			,ans:"あたたかい"	,tip:"warm"									,group:101	,accr:0},
+{que:"涼しい"			,ans:"すずしい"		,tip:"cool"									,group:101	,accr:0},
+{que:"暑い"				,ans:"あつい"		,tip:"hot (temperature)"					,group:101	,accr:0},
+{que:"寒い"				,ans:"さむい"		,tip:"cold (temperature)"					,group:101	,accr:0},
+{que:"熱い"				,ans:"あつい"		,tip:"hot (heat)"							,group:102	,accr:0},	//温い
+{que:"冷たい"			,ans:"つめたい"		,tip:"cold (heat)"							,group:102	,accr:0},
+{que:"新しい"			,ans:"あたらしい"	,tip:"new"									,group:103	,accr:0},
+{que:"古い"				,ans:"ふるい"		,tip:"old"									,group:103	,accr:0},
+{que:"赤い"				,ans:"あかい"		,tip:"red"									,group:104	,accr:0},
+{que:"黄色い"			,ans:"きいろい"		,tip:"yellow"								,group:104	,accr:0},
+{que:"忙しい"			,ans:"いそがしい"	,tip:"busy"									,group:105	,accr:0},
+{que:"暇な"				,ans:"ひまな"		,tip:"being bored"							,group:105	,accr:0},
+{que:"＿い"				,ans:"いい"			,tip:"good"									,group:106	,accr:0},
+{que:"嫌な"				,ans:"いやな"		,tip:"hateful"								,group:106	,accr:0},
+{que:"美味しい"			,ans:"おいしい"		,tip:"delicious"							,group:107	,accr:0},
+{que:"不味い"			,ans:"まずい"		,tip:"bad tasting"							,group:107	,accr:0},
+{que:"大きい"			,ans:"おおきい"		,tip:"big"									,group:108	,accr:0},
+{que:"小さい"			,ans:"ちいさい"		,tip:"small"								,group:108	,accr:0},
+{que:"遅い"				,ans:"おそい"		,tip:"late"									,group:109	,accr:0},
+{que:"早い"				,ans:"はやい"		,tip:"early"								,group:109	,accr:0},
+{que:"遅い"				,ans:"おそい"		,tip:"slow"									,group:110	,accr:0},
+{que:"速い"				,ans:"はやい"		,tip:"quick/fast"							,group:110	,accr:0},
+{que:"重い"				,ans:"おもい"		,tip:"heavy"								,group:111	,accr:0},
+{que:"軽い"				,ans:"かるい"		,tip:"light"								,group:111	,accr:0},
+{que:"面白い"			,ans:"おもしろい"	,tip:"interesting"							,group:112	,accr:0},
+{que:"詰まらない"		,ans:"つまらない"	,tip:"boring"								,group:112	,accr:0},
+{que:"汚い"				,ans:"きたない"		,tip:"dirty"								,group:113	,accr:0},	//清い
+{que:"綺麗な"			,ans:"きれいな"		,tip:"beautiful/clean"						,group:113	,accr:0},
+{que:"近い"				,ans:"ちかい"		,tip:"near"									,group:114	,accr:0},
+{que:"遠い"				,ans:"とおい"		,tip:"far"									,group:114	,accr:0},
+{que:"長い"				,ans:"ながい"		,tip:"long"									,group:115	,accr:0},
+{que:"短い"				,ans:"みじかい"		,tip:"short"								,group:115	,accr:0},
+{que:"太い"				,ans:"ふとい"		,tip:"thick/fat"							,group:116	,accr:0},
+{que:"細い"				,ans:"ほそい"		,tip:"thin"									,group:116	,accr:0},
+{que:"難しい"			,ans:"むずかしい"	,tip:"difficult"							,group:117	,accr:0},
+{que:"易しい"			,ans:"やさしい"		,tip:"easy"									,group:117	,accr:0},
 {que:"明るい"			,ans:"あかるい"		,tip:"bright"								,group:0	,accr:0},	//暗い
-{que:"赤い"				,ans:"あかい"		,tip:"red"									,group:0	,accr:0},
-{que:"暖かい"			,ans:"あたたかい"	,tip:"warm"									,group:0	,accr:0},	//寒い/冷たい
-{que:"新しい"			,ans:"あたらしい"	,tip:"new"									,group:0	,accr:0},	//古い
-{que:"暑い"				,ans:"あつい"		,tip:"hot (temperature)"					,group:0	,accr:0},	//寒い
-{que:"熱い"				,ans:"あつい"		,tip:"hot (heat)"							,group:0	,accr:0},	//冷たい/温い
 {que:"危ない"			,ans:"あぶない"		,tip:"dangerous"							,group:0	,accr:0},	//安全な
-{que:"＿い"				,ans:"いい"			,tip:"good"									,group:0	,accr:0},
-{que:"忙しい"			,ans:"いそがしい"	,tip:"busy"									,group:0	,accr:0},	//暇/暇な
 {que:"痛い"				,ans:"いたい"		,tip:"painful"								,group:0	,accr:0},
-{que:"嫌な"				,ans:"いやな"		,tip:"hateful"								,group:0	,accr:0},
-{que:"美味しい"			,ans:"おいしい"		,tip:"delicious"							,group:0	,accr:0},	//不味い
-{que:"大きい"			,ans:"おおきい"		,tip:"big"									,group:0	,accr:0},	//小さい
+{que:"辛い"				,ans:"からい"		,tip:"hot/spicy"							,group:0	,accr:0},	//甘い/苦い
+{que:"可愛い"			,ans:"かわいい"		,tip:"cute"									,group:0	,accr:0},	//憎い
+{que:"元気な"			,ans:"げんきな"		,tip:"energy/fine"							,group:0	,accr:0},
+{que:"怖い"				,ans:"こわい"		,tip:"frightening"							,group:0	,accr:0},
+{que:"静かな"			,ans:"しずかな"		,tip:"quiet"								,group:0	,accr:0},	//騒がしい
+{que:"狭い"				,ans:"せまい"		,tip:"narrow"								,group:0	,accr:0},	//広い
+{que:"大切な"			,ans:"たいせつな"	,tip:"important"							,group:0	,accr:0},	//粗末
+{que:"＿＿＿い"			,ans:"たいてい"		,tip:"usually"								,group:0	,accr:0},
+{que:"楽しい"			,ans:"たのしい"		,tip:"enjoyable"							,group:0	,accr:0},	//苦しい
+{que:"大好きな"			,ans:"だいすきな"	,tip:"like very much"						,group:0	,accr:0},
+{que:"強い"				,ans:"つよい"		,tip:"strong"								,group:0	,accr:0},	//弱い
+{que:"賑やかな"			,ans:"にぎやかな"	,tip:"lively"								,group:0	,accr:0},	//寂しい/淋しい
+{que:"低い"				,ans:"ひくい"		,tip:"low"									,group:0	,accr:0},	//高い
+{que:"便利な"			,ans:"べんりな"		,tip:"convenience"							,group:0	,accr:0},	//不便
+{que:"丸い"				,ans:"まるい"		,tip:"round"								,group:0	,accr:0},	//四角い
+{que:"円い"				,ans:"まるい"		,tip:"round"								,group:0	,accr:0},
+{que:"安い"				,ans:"やすい"		,tip:"cheap"								,group:0	,accr:0},	//高い
+{que:"有名な"			,ans:"ゆうめいな"	,tip:"famous"								,group:0	,accr:0},	//無名
+{que:"立派な"			,ans:"りっぱな"		,tip:"splendid"								,group:0	,accr:0},	//粗末/貧弱
+{que:"若い"				,ans:"わかい"		,tip:"young"								,group:0	,accr:0},	//老いた
+{que:"悪い"				,ans:"わるい"		,tip:"bad"									,group:0	,accr:0},	//良い/好い/善い
+//-----------------------------------------------------N-----------------------------------------------------
+{que:"明後日"			,ans:"あさって"		,tip:"the day after tomorrow"				,group:1001	,accr:0},
+{que:"明日"				,ans:"あした"		,tip:"tomorrow"								,group:1001	,accr:0},
+{que:"一昨日"			,ans:"おととい"		,tip:"the day before yesterday"				,group:1001	,accr:0},
+{que:"足"				,ans:"あし"			,tip:"leg/foot"								,group:1002	,accr:0},	//手/腕/冠
+{que:"頭"				,ans:"あたま"		,tip:"head"									,group:1002	,accr:0},	//尾/尻
+{que:"兄"				,ans:"あに"			,tip:"(my) brother"							,group:1003	,accr:0},
+{que:"姉"				,ans:"あね"			,tip:"(my) sister"							,group:1003	,accr:0},
+{que:"弟"				,ans:"おとうと"		,tip:"(your) younger brother"				,group:1003	,accr:0},
+{que:"妹"				,ans:"いもうと"		,tip:"younger sister"						,group:1003	,accr:0},
+{que:"お兄さん"			,ans:"おにいさん"	,tip:"(your) older brother"					,group:1003	,accr:0},
+{que:"お姉さん"			,ans:"おねえさん"	,tip:"(your) older sister"					,group:1003	,accr:0},
+{que:"お母さん"			,ans:"おかあさん"	,tip:"(your) mother"						,group:1004	,accr:0},
+{que:"お父さん"			,ans:"おとうさん"	,tip:"(your) father"						,group:1004	,accr:0},
+{que:"伯父"				,ans:"おじ"			,tip:"uncle"								,group:1005	,accr:0},
+{que:"伯母"				,ans:"おば"			,tip:"(my) aunt"							,group:1005	,accr:0},
+{que:"お爺さん"			,ans:"おじいさん"	,tip:"grand father"							,group:1006	,accr:0},
+{que:"お婆さん"			,ans:"おばあさん"	,tip:"(your) grand mother"					,group:1006	,accr:0},
+{que:"男"				,ans:"おとこ"		,tip:"man"									,group:1007	,accr:0},
+{que:"女"				,ans:"おんな"		,tip:"woman"								,group:1007	,accr:0},
+{que:"秋"				,ans:"あき"			,tip:"autumn/fall"							,group:0	,accr:0},	//春/夏/冬
+{que:"朝"				,ans:"あさ"			,tip:"morning"								,group:0	,accr:0},	//夕/晩
+{que:"朝御飯"			,ans:"あさごはん"	,tip:"breakfast"							,group:0	,accr:0},	//夕御飯/晩御飯
+{que:"雨"				,ans:"あめ"			,tip:"rain"									,group:0	,accr:0},	//晴れ
+{que:"家"				,ans:"いえ"			,tip:"house"								,group:0	,accr:0},
+{que:"＿＿"				,ans:"うち"			,tip:"home"									,group:0	,accr:0},
+{que:"池"				,ans:"いけ"			,tip:"pond"									,group:0	,accr:0},
+{que:"医者"				,ans:"いしゃ"		,tip:"doctor"								,group:0	,accr:0},	//患者
+{que:"椅子"				,ans:"いす"			,tip:"chair"								,group:0	,accr:0},
+{que:"今"				,ans:"いま"			,tip:"now"									,group:0	,accr:0},	//古/昔
+{que:"意味"				,ans:"いみ"			,tip:"meaning"								,group:0	,accr:0},
+{que:"入り口"			,ans:"いりぐち"		,tip:"entrance"								,group:0	,accr:0},	//出口
+{que:"色"				,ans:"いろ"			,tip:"color"								,group:0	,accr:0},
+{que:"歌"				,ans:"うた"			,tip:"song"									,group:0	,accr:0},
+{que:"海"				,ans:"うみ"			,tip:"sea"									,group:0	,accr:0},	//山/陸
+{que:"映画"				,ans:"えいが"		,tip:"movie"								,group:0	,accr:0},
+{que:"映画館"			,ans:"えいがかん"	,tip:"movie theater"						,group:0	,accr:0},
+{que:"英語"				,ans:"えいご"		,tip:"English"								,group:0	,accr:0},
+{que:"駅"				,ans:"えき"			,tip:"station"								,group:0	,accr:0},
+{que:"円"				,ans:"えん"			,tip:"yen"									,group:0	,accr:0},
+{que:"鉛筆"				,ans:"えんぴつ"		,tip:"pencil"								,group:0	,accr:0},
+{que:"お菓子"			,ans:"おかし"		,tip:"snacks"								,group:0	,accr:0},
+{que:"お金"				,ans:"おかね"		,tip:"money"								,group:0	,accr:0},
+{que:"奥さん"			,ans:"おくさん"		,tip:"(your) wife"							,group:0	,accr:0},
+{que:"お酒"				,ans:"おさけ"		,tip:"liquor"								,group:0	,accr:0},
+{que:"お皿"				,ans:"おさら"		,tip:"plate"								,group:0	,accr:0},
+{que:"押し入れ"			,ans:"おしいれ"		,tip:"closet"								,group:0	,accr:0},
+{que:"お茶"				,ans:"おちゃ"		,tip:"tea"									,group:0	,accr:0},
+{que:"お手洗い"			,ans:"おてあらい"	,tip:"toilet"								,group:0	,accr:0},
+{que:"一昨年"			,ans:"おととし"		,tip:"the year before last"					,group:0	,accr:0},
+{que:"大人"				,ans:"おとな"		,tip:"adult"								,group:0	,accr:0},	//子供
+{que:"男の子"			,ans:"おとこのこ"	,tip:"boy"									,group:0	,accr:0},	//女の子
+{que:"お腹"				,ans:"おなか"		,tip:"stomach"								,group:0	,accr:0},
+{que:"お弁当"			,ans:"おべんとう"	,tip:"lunch box"							,group:0	,accr:0},
+{que:"音楽"				,ans:"おんがく"		,tip:"music"								,group:0	,accr:0},
 {que:"＿"}
 );
